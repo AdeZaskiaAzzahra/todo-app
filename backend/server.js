bad64ab53a317a59ebff4d33d653b6f802717ac2
AdeZaskiaAzzahra <adezaskiaazzahra@gmail.com>
2026-05-02 18:24:33 +0700

    fix

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -7,10 +8,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.put("/toggle/:id", async (req, res) => {
-  const todo = await Todo.findById(req.params.id);
-  todo.completed = !todo.completed;
-  await todo.save();
-  res.send("Updated");
+const todo = await Todo.findById(req.params.id);
+todo.completed = !todo.completed;
+await todo.save();
+res.send("Updated");
 });
 
 // koneksi database
@@ -20,45 +21,45 @@ mongoose.connect("mongodb://adekynara_db_user:cwnYcYJlkBfOPe38@ac-1wchmxb-shard-
 
 // model
 const Todo = mongoose.model("Todo", {
-  text: String,
-  date: String,
-  completed: {
+text: String,
+date: String,
+completed: {
     type: Boolean,
     default: false
-  }
+}
 });
 
 // CREATE
 app.post("/add", async (req, res) => {
-  const newTodo = new Todo({
+const newTodo = new Todo({
     text: req.body.text,
     date: req.body.date
-  });
+});
 
-  await newTodo.save();
-  res.send("Added");
+await newTodo.save();
+res.send("Added");
 });
 
 // READ
 app.get("/get", async (req, res) => {
-  const todos = await Todo.find();
-  res.json(todos);
+const todos = await Todo.find();
+res.json(todos);
 });
 
 // DELETE
 app.delete("/delete/:id", async (req, res) => {
-  console.log("ID:", req.params.id);
+console.log("ID:", req.params.id);
 
-  try {
+try {
     await Todo.findByIdAndDelete(req.params.id);
     res.send("Deleted");
-  } catch (err) {
+} catch (err) {
     console.log(err);
     res.status(500).send("Error deleting");
-  }
+}
 });
 
 app.listen(5000, () => {
-  console.log("Server jalan di port 5000");
+console.log("Server jalan di port 5000");
 });
 
